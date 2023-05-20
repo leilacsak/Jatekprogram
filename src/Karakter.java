4953017af5b77452af613be60e4fca3fca72e33e
leilacsak <leilacsak@gmail.com>
2023-05-20 16:25:23 +0200

    Karakter javítva: Új HarcException

--- src/Karakter.java
+++ src/Karakter.java
@@ -2,28 +2,36 @@ import java.util.Random;
 
 class Karakter {
     protected int eletero;
+    private int pozicio;
 
-    public Karakter(){
+    public Karakter() {
         Random random = new Random();
         this.eletero = random.nextInt(6) + 4; // d6+3 életerő
     }
+
     public int getEletero() {
         return eletero;
     }
+
     public void setEletero(int eletero) {
-        if (eletero > 0) {
-            this.eletero = eletero;
-        } else {
-            this.eletero = 0;
-        }
+        this.eletero = Math.max(eletero, 0);
     }
-    public void tamad(Karakter karakter) throws HarcException {
+
+    public void tamad(Karakter karakter, StringBuilder jatekter, int ignoredUtkozetPozicio) throws HarcException {
         Random random = new Random();
         int sebzes = random.nextInt(6) + 1; // d6 sebzés
         karakter.setEletero(karakter.getEletero() - sebzes);
 
         if (karakter.getEletero() <= 0) {
-            throw new HarcException(karakter.getClass().getSimpleName() + " meghalt!");
+
+            if (karakter.getEletero() <= 0) {
+                if (jatekter.charAt(pozicio) != 'X') {
+                    throw new HarcException(jatekter.toString() + " --> H:" + getEletero() + ", V:" + karakter.getEletero() + " A " + karakter.getClass().getSimpleName() + " meghalt! ");
+                } else {
+                    throw new HarcException(jatekter.toString() + " --> Harc: H:" + getEletero() + ", V:" + karakter.getEletero() + " A " + karakter.getClass().getSimpleName() + " meghalt! ");
+                }
+
+            }
         }
     }
 }
