0e8775efbe12c83a9257ca2cf402ebc382dbf655
leilacsak <leilacsak@gmail.com>
2023-05-20 13:07:23 +0200

    Változtatás: Harcos és Varázsló öröklődnek a Karakterből.

--- src/Harcos.java
+++ src/Harcos.java
@@ -1,28 +1,3 @@
-import java.util.Random;
-// A harcos karaktere mint az egyik játékos.
-class Harcos {
-    private int eletero;
+class Harcos extends Karakter {
 
-    public Harcos() {
-        Random random = new Random();
-        this.eletero = random.nextInt(6) + 4; // d6+3 életerő
-    }
-
-    public int getEletero() {
-        return eletero;
-    }
-
-    public void setEletero(int eletero) {
-        if (eletero > 0) {
-            this.eletero = eletero;
-        } else {
-            this.eletero = 0;
-        }
-    }
-
-    public void tamad(Varazslo varazslo) {
-        Random random = new Random();
-        int sebzes = random.nextInt(6) + 1; // d6 sebzés
-        varazslo.setEletero(varazslo.getEletero() - sebzes);
-    }
 }
--- src/Varazslo.java
+++ src/Varazslo.java
@@ -1,28 +1,3 @@
-import java.util.Random;
-// A Varázsló karaktere mint a másik játékos.
-class Varazslo {
-    private int eletero;
+class Varazslo extends Karakter {
 
-    public Varazslo() {
-        Random random = new Random();
-        this.eletero = random.nextInt(6) + 4; // d6+3 életerő
-    }
-
-    public int getEletero() {
-        return eletero;
-    }
-
-    public void setEletero(int eletero) {
-        if (eletero > 0) {
-            this.eletero = eletero;
-        } else {
-            this.eletero = 0;
-        }
-    }
-
-    public void tamad(Harcos harcos) {
-        Random random = new Random();
-        int sebzes = random.nextInt(6) + 1; // d6 sebzés
-        harcos.setEletero(harcos.getEletero() - sebzes);
-    }
 }
