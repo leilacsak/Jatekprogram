bf45d5de5bc75dadaa104d1bef47635c053f1534
leilacsak <leilacsak@gmail.com>
2023-05-21 18:46:39 +0200

    A harcos és a Varazslo implementálja a Tamado interfészt

--- src/Harcos.java
+++ src/Harcos.java
@@ -1,5 +1,23 @@
-class Harcos extends Karakter {
+import java.util.Random;
+
+class Harcos extends Karakter implements Tamado {
     public Harcos() {
         super();
+    }
+
+    @Override
+    public void tamad(Karakter karakter, StringBuilder jatekter, int utkozetPozicio) throws HarcException {
+        // Harcos támadási logika
+        Random random = new Random();
+        int sebzes = random.nextInt(6) + 1; // d6 sebzés
+        karakter.setEletero(karakter.getEletero() - sebzes);
+
+        if (karakter.getEletero() <= 0) {
+            if (utkozetPozicio != -1) {
+                throw new HarcException(jatekter + " --> Harc: H:" + getEletero() + ", V:" + karakter.getEletero() + " A " + karakter.getClass().getSimpleName() + " meghalt! ");
+            } else {
+                throw new HarcException(jatekter + " --> H:" + getEletero() + ", V:" + karakter.getEletero() + " A " + karakter.getClass().getSimpleName() + " meghalt! ");
+            }
+        }
     }
 }
--- src/Varazslo.java
+++ src/Varazslo.java
@@ -1,5 +1,23 @@
-class Varazslo extends Karakter {
+import java.util.Random;
+
+class Varazslo extends Karakter implements Tamado {
     public Varazslo() {
         super();
+    }
+
+    @Override
+    public void tamad(Karakter karakter, StringBuilder jatekter, int utkozetPozicio) throws HarcException {
+        // Varázsló támadási logika
+        Random random = new Random();
+        int sebzes = random.nextInt(4) + 1; // d4 sebzés
+        karakter.setEletero(karakter.getEletero() - sebzes);
+
+        if (karakter.getEletero() <= 0) {
+            if (utkozetPozicio != -1) {
+                throw new HarcException(jatekter + " --> Harc: H:" + karakter.getEletero() + ", V:" + getEletero() + " A " + karakter.getClass().getSimpleName() + " meghalt! ");
+            } else {
+                throw new HarcException(jatekter + " --> H:" + karakter.getEletero() + ", V:" + getEletero() + " A " + karakter.getClass().getSimpleName() + " meghalt! ");
+            }
+        }
     }
 }
