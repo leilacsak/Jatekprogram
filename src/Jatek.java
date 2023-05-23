f07011346466f4432fb681b21d1adf47b149a1d5
leilacsak <leilacsak@gmail.com>
2023-05-23 18:58:22 +0200

    A játékmenet megejelenik GUI-n is.

--- src/Jatek.java
+++ src/Jatek.java
@@ -12,19 +12,42 @@ import static javax.imageio.ImageIO.read;
 class Jatek extends JPanel implements ActionListener {
     private final Harcos harcos;
     private final Varazslo varazslo;
-    private final JLabel allapotLabel;
+    private JLabel allapotLabel;
     private final Timer idozito;
     private BufferedImage varazsloImage;
+    private StringBuilder jatekmenet;
+    private JTextArea jatekmenetTextArea;
+
 
 
     public Jatek(Harcos harcos, Varazslo varazslo) {
         this.harcos = harcos;
         this.varazslo = varazslo;
+
         setLayout(new BorderLayout());
         JLabel jatekterLabel = new JLabel();
         allapotLabel = new JLabel();
+        jatekmenetTextArea = new JTextArea();
+        JScrollPane jatekmenetScrollPane = new JScrollPane(jatekmenetTextArea);
         add(jatekterLabel, BorderLayout.CENTER);
         add(allapotLabel, BorderLayout.SOUTH);
+        add(jatekmenetScrollPane, BorderLayout.EAST);
+        setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+
+        jatekterLabel = new JLabel();
+        allapotLabel = new JLabel();
+        jatekmenetTextArea = new JTextArea();
+        jatekmenetTextArea.setEditable(false);
+
+        add(jatekterLabel, gbc);
+        gbc.gridy++;
+        add(allapotLabel, gbc);
+        gbc.gridy++;
+        add(jatekmenetTextArea, gbc);
+
 
         idozito = new Timer(1000, this);
         idozito.start();
@@ -35,9 +58,10 @@ class Jatek extends JPanel implements ActionListener {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        jatekmenet = new StringBuilder();
     }
     public void game() {
-
+        jatekmenet = new StringBuilder();
         int utkozetPozicio;
         while (true) {
             try {
@@ -53,8 +77,10 @@ class Jatek extends JPanel implements ActionListener {
                     jatekter.setCharAt(harcosPozicio, 'X');
                     harcos.tamad(varazslo, jatekter, utkozetPozicio);
                     varazslo.tamad(harcos, jatekter, utkozetPozicio);
+                    jatekmenet.append(jatekter).append(" --> Harc: H:").append(harcos.getEletero()).append(", V:").append(varazslo.getEletero()).append("\n");
                     System.out.println(jatekter + " --> Harc: H:" + harcos.getEletero() + ", V:" + varazslo.getEletero());
                 } else {
+                    jatekmenet.append(jatekter).append(" --> H:").append(harcos.getEletero()).append(", V:").append(varazslo.getEletero()).append("\n");
                     System.out.println(jatekter + " --> H:" + harcos.getEletero() + ", V:" + varazslo.getEletero());
                 }
 
@@ -62,6 +88,7 @@ class Jatek extends JPanel implements ActionListener {
                     break;
                 }
             } catch (HarcException e) {
+                jatekmenet.append(e.getMessage());
                 System.out.println(e.getMessage());
                 break;
             }
@@ -72,10 +99,16 @@ class Jatek extends JPanel implements ActionListener {
         } else {
             allapot = "Game over...";
         }
-        allapotLabel.setText(allapot);
-        idozito.stop();
-    }
 
+        allapotLabel.setText(allapot);
+        allapotLabel.setVisible(true);
+        idozito.stop();
+
+        jatekmenetTextArea.setText(jatekmenet.toString());
+        jatekmenetTextArea.setEditable(false);
+        jatekmenetTextArea.setVisible(true);
+
+    }
     @Override
     public void actionPerformed(ActionEvent e) {
         repaint();
@@ -103,7 +136,7 @@ class Jatek extends JPanel implements ActionListener {
         // A harcos bal oldalra rajzolása
         int harcosX = getWidth() / 4 - 25 - 50;
         int harcosY = (getHeight() - 100) / 2;
-        g2d.drawImage(harcosImage, harcosX, harcosY, 100, 200, null);
+        g2d.drawImage(harcosImage, harcosX, harcosY, 150, 200, null);
 
         URL varazsloImageURL = getClass().getResource("/varazslo.png");
         try {
@@ -116,7 +149,17 @@ class Jatek extends JPanel implements ActionListener {
         // A varázsló jobb oldalra rajzolása
         int varazsloX = 3 * getWidth() / 4 - 25 + 50;
         int varazsloY = (getHeight() - 100) / 2;
-        g2d.drawImage(varazsloImage, varazsloX, varazsloY, 100, 200, null);
+        g2d.drawImage(varazsloImage, varazsloX, varazsloY, 150, 200, null);
+
+        // Játékmenet szövegének elhelyezése középen
+        Font font = new Font("Arial", Font.PLAIN, 16);
+        g2d.setFont(font);
+        FontMetrics fontMetrics = g2d.getFontMetrics(font);
+        int textWidth = fontMetrics.stringWidth(jatekmenetTextArea.getText());
+        int textHeight = fontMetrics.getHeight();
+        int textX = (getWidth() - textWidth) / 2;
+        int textY = (getHeight() + harcosImage.getHeight()) / 2 + textHeight;
+        g2d.drawString(jatekmenetTextArea.getText(), textX, textY);
     }
 }
 
