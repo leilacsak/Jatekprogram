98878ab4c1b435dc0c7cac1d57c7ad015f34f57d
leilacsak <leilacsak@gmail.com>
2023-05-23 18:16:08 +0200

    Képek benne vannak a GUI-ban.

--- src/Jatek.java
+++ src/Jatek.java
@@ -2,32 +2,41 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Objects;
 import java.util.Random;
+import static javax.imageio.ImageIO.read;
 
 class Jatek extends JPanel implements ActionListener {
     private final Harcos harcos;
     private final Varazslo varazslo;
-    private final JLabel jatekterLabel;
     private final JLabel allapotLabel;
     private final Timer idozito;
+    private BufferedImage varazsloImage;
+
 
     public Jatek(Harcos harcos, Varazslo varazslo) {
         this.harcos = harcos;
         this.varazslo = varazslo;
         setLayout(new BorderLayout());
-        jatekterLabel = new JLabel();
+        JLabel jatekterLabel = new JLabel();
         allapotLabel = new JLabel();
         add(jatekterLabel, BorderLayout.CENTER);
         add(allapotLabel, BorderLayout.SOUTH);
 
-        idozito = new Timer( 1000, this);
+        idozito = new Timer(1000, this);
         idozito.start();
 
+        try {
+            read(Objects.requireNonNull(getClass().getResource("/harcos.png")));
+            varazsloImage = read(Objects.requireNonNull(getClass().getResource("/varazslo.png")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-
     public void game() {
-
-        jatekterLabel.setText("Start!");
 
         int utkozetPozicio;
         while (true) {
@@ -61,22 +70,64 @@ class Jatek extends JPanel implements ActionListener {
         if (harcos.getEletero() <= 0 && varazslo.getEletero() <= 0) {
             allapot = "Küzdelem vége: Döntetlen!";
         } else {
-        allapot = "Game over...";
-    }
+            allapot = "Game over...";
+        }
         allapotLabel.setText(allapot);
         idozito.stop();
-}
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
-    // A harc állapotát frissíti az időzítő esemény.
         repaint();
     }
+
+
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        // A játékmezőt rajzolja ki
-        // Itt meg kell valósítani a grafikus kirajzolást a játékmezőnek.
+
+        Graphics2D g2d = (Graphics2D) g;
+
+        g2d.setColor(Color.WHITE);
+        g2d.fillRect(0, 0, getWidth(), getHeight());
+
+        URL harcosImageURL = getClass().getResource("/harcos.png");
+        BufferedImage harcosImage;
+        try {
+            assert harcosImageURL != null;
+            harcosImage = read(harcosImageURL);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        // A harcos bal oldalra rajzolása
+        int harcosX = getWidth() / 4 - 25 - 50;
+        int harcosY = (getHeight() - 100) / 2;
+        g2d.drawImage(harcosImage, harcosX, harcosY, 100, 200, null);
+
+        URL varazsloImageURL = getClass().getResource("/varazslo.png");
+        try {
+            assert varazsloImageURL != null;
+            varazsloImage = read(varazsloImageURL);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        // A varázsló jobb oldalra rajzolása
+        int varazsloX = 3 * getWidth() / 4 - 25 + 50;
+        int varazsloY = (getHeight() - 100) / 2;
+        g2d.drawImage(varazsloImage, varazsloX, varazsloY, 100, 200, null);
     }
 }
+
+
+
+
+
+
+
+
+
+
 
 
--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,12 @@ public class Main {
             Harcos harcos = new Harcos();
             Varazslo varazslo = new Varazslo();
 
-            JFrame frame = new JFrame("Jatekprogram GUI");
+            JFrame frame = new JFrame("Jatekprogram");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(800, 600);
+            frame.setLocationRelativeTo(null);
+            frame.setResizable(false);
+
             Jatek jatekPanel = new Jatek(harcos, varazslo);
             frame.add(jatekPanel);
 
@@ -18,8 +23,5 @@ public class Main {
             frame.setVisible(true);
         });
 
-
-
     }
 }
-
